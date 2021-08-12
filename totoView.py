a152cd01304c9351ee617848e5d12d5185f19c35
Remy Zyngfogel <remy@system76-pc.localdomain>
2021-08-13 09:45:02 +1200

    new args

--- totoView.py
+++ totoView.py
@@ -3,9 +3,17 @@ from __future__ import absolute_import
 import sys
 
 
-def main(filenames=[],dataframes=None):
+def main(filenames=[],reader=None,dataframes=None):
     import totoview
-    totoview.show(filenames=filenames,dataframe=dataframes)
+    totoview.show(filenames=filenames,dataframe=dataframes,reader=reader)
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    import argparse
+    parser = argparse.ArgumentParser(prog='totoView.py', usage='%(prog)s file1.txt [options]')
+    ## main arguments
+    parser.add_argument('files', type=str,nargs='+',default=None,help='name of the files to read')
+    ## options
+    parser.add_argument('--reader','-r',type=str,default=None,help='name of the reader to use')
+
+    args = parser.parse_args()
+    main(filenames=args.files,reader=args.reader)
